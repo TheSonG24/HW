4d1d0e8621c87448cadcb9792fcd62f81f52b7eb
Son <147977224+TheSonG24@users.noreply.github.com>
2024-11-13 17:09:13 +0300

    Update 5.py

--- HW (12.10.2024)/5.py
+++ HW (12.10.2024)/5.py
@@ -1,2 +1,24 @@
-d = '23235'
-print(str(d), d)
+def f(p):
+    s = ''
+    while p != 0:
+        s += str(p % 3)
+        p = p // 3
+    return s[:: -1]
+
+
+e = []
+for n in range(1, 10000):
+    n = f(n)
+    n = n.replace('1', 'a')
+    n = n.replace('2', 'b')
+    n = n.replace('0', 'c')
+    n = n.replace('a', '2')
+    n = n.replace('b', '0')
+    n = n.replace('c', '1')
+    n = int(n)
+    pp = str(n)
+    if n != 0:
+        R = pp[:: -1] + f(pp.count('1')+pp.count('2')*2)
+        if int(R, 3) > 10 ** 4:
+            e.append(int(R, 3))
+print(min(e))
